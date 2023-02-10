b616e7f6907956dbe78a9133d5625ee9b38f0e24
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-02-10 17:59:08 -0300

    button structure for custom styles

--- src/components/main-button/MainButton.tsx
+++ src/components/main-button/MainButton.tsx
@@ -3,10 +3,11 @@ import style from "./style.module.css";
 
 type Props = {
 	text: string;
+	styles?: string;
 };
 
-function MainButton({ text }: Props) {
-	return <div className={style.box}>{text}</div>;
+function MainButton({ text, styles }: Props) {
+	return <div className={`${style.box} ${styles}`}>{text}</div>;
 }
 
 export default MainButton;
