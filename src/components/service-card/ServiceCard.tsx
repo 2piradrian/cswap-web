1595214c9c1ca381212657d36f3d7b6455000ef3
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-02-17 16:59:35 -0300

    url added in service card

--- src/components/service-card/ServiceCard.tsx
+++ src/components/service-card/ServiceCard.tsx
@@ -4,17 +4,22 @@ import style from "./style.module.css";
 type Props = {
 	title: string;
 	description: string;
+	target: string;
 	url: string;
 };
 
-function ServiceCard({ title, description, url }: Props) {
+function ServiceCard({ title, description, url, target }: Props) {
 	return (
 		<div className={style.box}>
 			<h3 className={style.title}>{title}</h3>
 
 			<p className={style.description}>{description}</p>
 
-			<InvisibleAnchor styles={style.hire} label="contratar">
+			<InvisibleAnchor
+				styles={style.hire}
+				label="contratar"
+				url={url}
+				target={target}>
 				<p>Contratar</p>
 			</InvisibleAnchor>
 		</div>
--- src/sections/services/Services.tsx
+++ src/sections/services/Services.tsx
@@ -8,17 +8,20 @@ function Services() {
 				<ServiceCard
 					title="Diseño de marca"
 					description="Definí la identidad de tu marca"
-					url=""
+					target="_blank"
+					url="https://api.whatsapp.com/send?phone=543516135462&text=Hola%2C%20escribo%20desde%20la%20p%C3%A1gina%20de%20c-swap.%20Me%20gustar%C3%ADa%20contratar%20el%20siguiente%20servicio%3A%20(Dise%C3%B1o%20de%20marca)."
 				/>
 				<ServiceCard
 					title="Diseño UX/UX"
 					description="Diseño de experiencia de usuario e interfaces"
-					url=""
+					target="_blank"
+					url="https://api.whatsapp.com/send?phone=543516135462&text=Hola%2C%20escribo%20desde%20la%20p%C3%A1gina%20de%20c-swap.%20Me%20gustar%C3%ADa%20contratar%20el%20siguiente%20servicio%3A%20(Dise%C3%B1o%20UX%2FUI)."
 				/>
 				<ServiceCard
 					title="Desarrollo"
 					description="Llevá a la práctica tu idea, nosotros la programamos"
-					url=""
+					target="_blank"
+					url="https://api.whatsapp.com/send?phone=543516135462&text=Hola%2C%20escribo%20desde%20la%20p%C3%A1gina%20de%20c-swap.%20Me%20gustar%C3%ADa%20contratar%20el%20siguiente%20servicio%3A%20(Dise%C3%B1o%20y%20desarrollo)."
 				/>
 			</div>
 		</div>
