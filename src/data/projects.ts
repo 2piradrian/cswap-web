ed0d17cded27721142a06e931fd78fb0521f2105
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-01-07 19:05:05 -0300

    Add ProjectCard component and display projects

---
+++ src/data/projects.ts
@@ -0,0 +1,32 @@
+export const projects = [
+	{
+		id: 1,
+		name: "Project 1",
+		description: "This is project 1",
+		image: "@/assets/projects/1.jpg",
+	},
+	{
+		id: 1,
+		name: "Project 1",
+		description: "This is project 1",
+		image: "@/assets/projects/1.jpg",
+	},
+	{
+		id: 1,
+		name: "Project 1",
+		description: "This is project 1",
+		image: "@/assets/projects/1.jpg",
+	},
+	{
+		id: 1,
+		name: "Project 1",
+		description: "This is project 1",
+		image: "@/assets/projects/1.jpg",
+	},
+	{
+		id: 1,
+		name: "Project 1",
+		description: "This is project 1",
+		image: "@/assets/projects/1.jpg",
+	},
+];
--- src/sections/all-projects/AllProjects.tsx
+++ src/sections/all-projects/AllProjects.tsx
@@ -1,3 +1,5 @@
+import ProjectCard from "@/components/project-card/ProjectCard";
+import { projects } from "@/data/projects";
 import style from "./style.module.css";
 
 export default function AllProjects() {
@@ -5,7 +7,18 @@ export default function AllProjects() {
 		<section className={`${style.container} section`}>
 			<div className={`${style.container} delimiter`}>
 				<h1>Proyectos</h1>
-				<div className={style.projectContainer}></div>
+				<div className={style.projectContainer}>
+					{projects.map((project: any) => {
+						return (
+							<ProjectCard
+								key={project.id}
+								description={project.description}
+								image={project.image}
+								title={project.name}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		</section>
 	);
